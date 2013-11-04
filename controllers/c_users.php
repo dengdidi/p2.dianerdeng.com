975c335c75b56921c2b91f5fd789d22536eb191b
Diane Deng <didi.deng@gmail.com>
2013-11-03 20:42:51 -0500

    modified users controller

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -12,6 +12,21 @@ class users_controller extends base_controller {
 
     public function signup() {
         echo "This is the signup page";
+        # Setup view
+            $this->template->content = View::instance('v_users_signup');
+            $this->template->title   = "Sign Up";
+
+        # Render template
+            echo $this->template;
+
+    }
+
+    public function p_signup() {
+
+        # Dump out the results of POST to see what the form submitted
+        echo '<pre>';
+        print_r($_POST);
+        echo '</pre>';                  
     }
 
     public function login() {
